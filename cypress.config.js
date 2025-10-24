76b00752ffde00bc790bbc97253c3d8fdb056cfb
Rafael Lima <rafaelLima@MBP-de-MacBook.Home>
2025-10-24 01:17:14 +0100

    Adding mochawesome report

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl:'http://localhost:4000/',
+    baseUrl: 'http://localhost:4000/',
+    reporter: 'cypress-mochawesome-reporter',
     video: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
